a7e50f248bb6768fd6d2da83f71697a37f01a547
Lakshmi1522 <112838460+Lakshmi1522@users.noreply.github.com>
2022-09-20 11:21:55 +0530

    Add files via upload

---
+++ leap.py
@@ -0,0 +1,10 @@
+n=float(input("enter the year"))
+if(n==0):
+    print("the year is invalid")
+elif(n%4==0):
+    print("the year is leap")
+else:
+    print("it is not a leap year")
+
+        
+        
